a239b778e9719865687b332bbdd8b79d3ee8058b
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-08-01 17:33:21 -0700

    adding scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,9 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :attendances, foreign_key: 'attended_event_id'
   has_many :attendees, through: :attendances, source: :attendee
+
+  default_scope -> { order(date_event: :asc) }
+  scope :past_event, -> {where('date_event < ?',  DateTime.current) }
+  scope :upcoming_event, -> {where('date_event >= ?', DateTime.current) }
+
 end
