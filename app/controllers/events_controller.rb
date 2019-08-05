184af28d33b96bd7bc57241d454bca8965ddec07
Santiago Torres G <thdealer@gmail.com>
2019-08-05 18:59:51 -0500

    passing invitations to event/show view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,12 @@
 class EventsController < ApplicationController
   def new
-    @event = Event.new()
+    @event = Event.new
   end
 
   def create
     @event = current_user.events.build(event_params)
     if @event.save
-      asist = current_user.attendances.build({attended_event_id: @event.id}).save
+      current_user.attendances.build( { attended_event_id: @event.id } ).save
       flash[:success] = 'Event Created'
       redirect_to events_index_path
     else
@@ -17,6 +17,10 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find_by_id(params[:id])
+    invited = Invitation.all.where(invited_event_id: params[:id])
+    ids = invited.map { |i| i[:guest_id] }
+    ids << current_user.id
+    @users = User.all.where.not(id: [ids])
   end
 
   def index
--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -3,8 +3,9 @@
 class InvitationsController < ApplicationController
   def new
     @users = User.all.where.not(id: current_user.id)
-    @temp = Hash.new
-    @events = current_user.events.each {|i| @temp[i.title] = i.id }
+    @events = {}
+    current_user.events.each { |i| @events[i.title] = i.id }
+
     @invitation = Invitation.new
     if current_user.nil?
       flash[:danger] = 'You have to login to invite someone to an event!'
@@ -13,19 +14,16 @@ class InvitationsController < ApplicationController
       #   @invitation = Invitation.new()
     end
   end
-#user = User.all.where.not(id: [1,3])
-#Invitation.all.where(guest_id:3, invited_event_id: 2)
+
   def create
     counter = 0
+    byebug
     guests = invitation_params[:guest_id].reject(&:blank?)
-    
+
     guests.each do |i|
-      # invited = Invitation.all.where(invited_event_id: 8)
-      # ids = invited.map {|i| i[:guest_id]}
-      # @users = User.all.where.not(ids)  
-###
-      valid = Invitation.find_by(guest_id: i.to_i, invited_event_id: invitation_params[:invited_event_id].to_i)
-      @invitation = Invitation.new(guest_id: i.to_i, invited_event_id: invitation_params[:invited_event_id].to_i) unless valid
+      found = Invitation.find_by(guest_id: i.to_i, invited_event_id: invitation_params[:invited_event_id].to_i)
+      byebug
+      @invitation = Invitation.new(guest_id: i.to_i, invited_event_id: invitation_params[:invited_event_id].to_i) if found.nil?
       counter += 1 if @invitation.save
     end
 
