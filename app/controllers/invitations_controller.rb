3a81e21a07c7df4c6eca8004086a5f48a636cc2d
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-08-05 12:25:41 -0700

    adding view restrictions to cards

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -2,6 +2,9 @@
 
 class InvitationsController < ApplicationController
   def new
+    @users = User.all.where.not(id: current_user.id)
+    @temp = Hash.new
+    @events = current_user.events.each {|i| @temp[i.title] = i.id }
     @invitation = Invitation.new
     if current_user.nil?
       flash[:danger] = 'You have to login to invite someone to an event!'
@@ -10,13 +13,19 @@ class InvitationsController < ApplicationController
       #   @invitation = Invitation.new()
     end
   end
-
+#user = User.all.where.not(id: [1,3])
+#Invitation.all.where(guest_id:3, invited_event_id: 2)
   def create
     counter = 0
     guests = invitation_params[:guest_id].reject(&:blank?)
-
+    
     guests.each do |i|
-      @invitation = Invitation.new(guest_id: i.to_i, invited_event_id: invitation_params[:invited_event_id].to_i)
+      # invited = Invitation.all.where(invited_event_id: 8)
+      # ids = invited.map {|i| i[:guest_id]}
+      # @users = User.all.where.not(ids)  
+###
+      valid = Invitation.find_by(guest_id: i.to_i, invited_event_id: invitation_params[:invited_event_id].to_i)
+      @invitation = Invitation.new(guest_id: i.to_i, invited_event_id: invitation_params[:invited_event_id].to_i) unless valid
       counter += 1 if @invitation.save
     end
 
