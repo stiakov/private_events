8c856a639feb8a1db761511e121e6855bb00e487
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-08-06 15:25:11 -0700

    updating validations for invitations view, deleting unused code, refactor existing code

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,35 +1,23 @@
 # frozen_string_literal: true
 
 class InvitationsController < ApplicationController
-  def new
-    @users = User.all.where.not(id: current_user.id)
-    @events = {}
-    current_user.events.each { |i| @events[i.title] = i.id }
-
-    @invitation = Invitation.new
-    if current_user.nil?
-      flash[:danger] = 'You have to login to invite someone to an event!'
-      redirect_to login_path
-      # else
-      #   @invitation = Invitation.new()
-    end
-  end
+  def new;end
 
   def create
     counter = 0
     guests = invitation_params[:guest_id].reject(&:blank?)
-
     guests.each do |i|
       found = Invitation.find_by(guest_id: i.to_i, invited_event_id: invitation_params[:invited_event_id].to_i)
       @invitation = Invitation.new(guest_id: i.to_i, invited_event_id: invitation_params[:invited_event_id].to_i) if found.nil?
       counter += 1 if @invitation.save
     end
 
-    if counter == guests.size
+    if guests.size > 0 && counter == guests.size
       flash[:success] = 'Invitations sent!'
       redirect_to users_show_path
     else
-      render json: { status: '404 Something went wrong' }
+      flash[:danger] = 'You have to select a guest, please check'
+      redirect_to myevents_path
     end
   end
 
