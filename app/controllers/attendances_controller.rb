fb85aa693b95a5ede8de254c791889b2fc2ca09a
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-08-14 12:13:37 -0700

    Modifying create and destroy methods

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -7,6 +7,7 @@ class AttendancesController < ApplicationController
     @attendance = Attendance.new(attendance_params)
     if @attendance.save
       event = Event.find_by_id(params[:attended_event_id])
+      invitation_accepted?(event, true)
       flash[:success] = "You are scheduled for #{event.title.upcase}"
       redirect_to events_path
     else
@@ -16,12 +17,21 @@ class AttendancesController < ApplicationController
   end
 
   def destroy
-    @attendance = current_user.attendances.find_by(attended_event_id: params[:attended_event_id])
-    flash[:success]="Attendance cancelled" if @attendance.delete 
+    # @attendance = current_user.attendances.find_by(attended_event_id: params[:attended_event_id])
+    event = Event.find_by_id(params[:attended_event_id])
+    invitation_accepted?(event, false)
+    # invitation_accepted?(@attendance.attended_event_id, false)
+    flash[:success]="Attendance cancelled" if event.delete 
     redirect_to events_path
   end
 
   private
+
+  def invitation_accepted?(event, val)
+    temp= current_user.invitations.find_by(invited_event_id: event)
+    temp.acceptance = val
+    temp.save
+  end
 
   def attendance_params
     params.permit(:attendee_id, :attended_event_id)
