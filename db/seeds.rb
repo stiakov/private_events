ed8721ebef39c58961bf07c1d2634a0e7a04147e
Santiago Torres G <thdealer@gmail.com>
2019-08-13 19:22:20 -0500

    changing seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,16 +3,16 @@
 emails = ['eli@woof.com', 'santiago@woof.com', 'dulce@woof.com']
 
 3.times do |i|
-  user = User.create(name: Faker::Name.name,
+  user = User.create(name: Faker::Artist.name,
                      username: Faker::Name.first_name,
                      email: emails[i])
 
   5.times do
     user.events.build(
-      title: Faker::Lorem.sentence,
+      title: Faker::Quotes::Shakespeare.hamlet_quote,
       date_event: Faker::Time.between(from: 3.months.ago, to: 1.month.from_now, format: :default),
-      description: Faker::Lorem.paragraph,
-      place: Faker::University.name,
+      description: Faker::Hacker.say_something_smart,
+      place: Faker::University.name
     ).save
     user.attendances.build(attended_event_id: Event.last.id).save
   end
