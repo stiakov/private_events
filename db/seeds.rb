5e504de4d6b7612f41925c8beec8088bead10341
Santiago Torres G <thdealer@gmail.com>
2019-08-16 10:03:27 -0500

    updating seeds, minor updates in users#index :white_check_mark: :rocket:

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
-emails = ['eli@woof.com', 'santiago@woof.com', 'dulce@woof.com']
+emails = %w[eli@woof.com santiago@woof.com dulce@woof.com]
 
 3.times do |i|
-  user = User.create(name: Faker::Artist.name,
+  user = User.create(name: Faker::DcComics.unique.name,
                      username: Faker::Name.first_name,
                      email: emails[i])
 
   5.times do
     user.events.build(
-      title: Faker::Quotes::Shakespeare.hamlet_quote,
-      date_event: Faker::Time.between(from: 3.months.ago, to: 1.month.from_now, format: :default),
-      description: Faker::Hacker.say_something_smart,
+      title: Faker::Hacker.say_something_smart,
+      date_event: Faker::Time.between(from: 3.months.ago, to: 2.month.from_now, format: :default),
+      description: Faker::Lorem.paragraph_by_chars,
       place: Faker::University.name
     ).save
     user.attendances.build(attended_event_id: Event.last.id).save
